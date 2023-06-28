4c234d82c89d9dfc8a39ca0ec66dcdfc4065ac1e
cod47516 <cod47516@adobe.com>
2023-06-28 13:47:26 +0200

    performance improvement

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -11,6 +11,9 @@ export default async function init(el) {
   const helloEl = createTag('h2', { class: 'hello-title' }, `${hello},`);
   const nameEl = createTag('p', { class: 'hello-name' }, name);
   el.append(helloEl, nameEl);
-  if (textArr[0]) window.gsap.to(helloEl, { x: 200 });
-  if (textArr[1]) window.gsap.to(nameEl, { x: 400 });
+  if (textArr[0] || textArr[1]) {
+    await loadScript('/deps/gsap.min.js');
+    if (textArr[0]) window.gsap.to(helloEl, { x: 200 });
+    if (textArr[1]) window.gsap.to(nameEl, { x: 400 });
+  }
 }
